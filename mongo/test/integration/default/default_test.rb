20a7817334ba81bba491499945f6e1f889058f9d
maroua-akkari <49191068+maroua-akkari@users.noreply.github.com>
2019-05-23 03:48:38 +0100

    tests if listening to mongodb port 27017

--- mongo/test/integration/default/default_test.rb
+++ mongo/test/integration/default/default_test.rb
@@ -1,9 +1,4 @@
-# # encoding: utf-8
 
-# Inspec test for recipe mongo::default
-
-# The Inspec reference, with examples and extensive documentation, can be
-# found at http://inspec.io/docs/reference/resources/
 
 unless os.windows?
   # This is an example test, replace with your own test.
