08e43f4461d5a0385057f4f9693974ca86400655
maroua-akkari <49191068+maroua-akkari@users.noreply.github.com>
2019-05-23 03:49:34 +0100

    apt and sc-mongodb dependancies

--- mongo/metadata.rb
+++ mongo/metadata.rb
@@ -7,16 +7,5 @@ long_description 'Installs/Configures mongo'
 version '0.1.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/mongo/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/mongo'
 depends 'apt'
 depends 'sc-mongodb', '~> 1.2.0'
